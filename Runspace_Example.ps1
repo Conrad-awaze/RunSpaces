ef4b8fca904ddf8fc48407d256c156fef75fe07b
Conrad Gauntlett <conrad.gauntlett@awaze.com>
2023-06-29 19:18:28 +0000

    updates

--- Runspace_Example.ps1
+++ Runspace_Example.ps1
@@ -1,1 +1,35 @@
-get-runspace
+Get-Runspace
+
+$Runspace = [runspacefactory]::CreateRunspace()
+$PowerShell = [powershell]::Create()
+
+$PowerShell.Runspace = $Runspace
+$Runspace.Open()
+$PowerShell.AddScript({Start-Sleep 5})
+
+$Job = $PowerShell.BeginInvoke()
+$PowerShell.EndInvoke($Job)
+$Runspace.Close()
+
+
+$Scriptblock = {
+    param($Name)
+    New-Item -Name $Name -ItemType File
+}
+
+$MaxThreads = 5
+$RunspacePool = [runspacefactory]::CreateRunspacePool(1, $MaxThreads)
+$RunspacePool.Open()
+$Jobs = @()
+
+1..10 | Foreach-Object {
+	$PowerShell = [powershell]::Create()
+	$PowerShell.RunspacePool = $RunspacePool
+	$PowerShell.AddScript($ScriptBlock).AddArgument($_)
+	$Jobs += $PowerShell.BeginInvoke()
+}
+
+while ($Jobs.IsCompleted -contains $false) {
+	Start-Sleep 1
+}
+
