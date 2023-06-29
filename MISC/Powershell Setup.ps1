2fb75c17d408574435170d39670e7eeafe333068
Conrad  Gauntlett <conrad.gauntlett@awaze.com>
2023-06-29 13:39:59 +0000

    git test

--- MISC/Powershell Setup.ps1
+++ MISC/Powershell Setup.ps1
@@ -5,3 +5,6 @@ curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --yes --dearmo
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/microsoft-debian-bullseye-prod bullseye main" > /etc/apt/sources.list.d/microsoft.list'
 
 sudo apt update && sudo apt install -y powershell
+
+# git test
+
