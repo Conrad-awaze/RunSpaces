c0429d41a89816ac45e50d519087e223253ee090
Conrad Gauntlett <conrad.gauntlett@awaze.com>
2023-06-29 19:04:14 +0100

    new script

---
+++ Rubspace_Example2.ps1
@@ -0,0 +1,23 @@
+# https://adamtheautomator.com/powershell-multithreading/#Create_the_Runspace
+
+$Scriptblock = {
+    param($Name)
+    New-Item -Name $Name -ItemType File
+}
+
+$MaxThreads = 5
+$RunspacePool = [runspacefactory]::CreateRunspacePool(1, $MaxThreads)
+$RunspacePool.Open()
+$Jobs = @()
+
+1..10 | Foreach-Object {
+	$PowerShell = [powershell]::Create()
+	$PowerShell.RunspacePool = $RunspacePool
+	$PowerShell.AddScript($ScriptBlock).AddArgument($_)
+	$Jobs += $PowerShell.BeginInvoke()
+}
+
+while ($Jobs.IsCompleted -contains $false) {
+	Start-Sleep 1
+}
+
